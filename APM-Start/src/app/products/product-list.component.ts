06b886194a68f02ef098235d33f780237fd01c81
Sathy Pannirselvam <spannirselvam@us.ci.org>
2019-05-12 22:22:59 -0600

    Services

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product'
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -24,32 +25,11 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://clipart-library.com/images/kcM8R5kcj.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://clipart-library.com/images/6ir5yddBT.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts =this.products;
-        this.listFilter ='cart';
+    constructor(private productService: ProductService) {
+        
+        
     }
 
     onRatingClicked(message: string) : void {
@@ -67,6 +47,7 @@ export class ProductListComponent implements OnInit {
     }
     ngOnInit(): void {
         //throw new Error("Method not implemented.");
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts =this.products;
     }
 }
